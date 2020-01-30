770482309e1b60ae352a86248f198af1d0692e05
yutveg <kodi.0n31l@gmail.com>
2020-01-30 14:05:24 -0800

    mvp testing met model todo

--- server/api/router/router.test.js
+++ server/api/router/router.test.js
@@ -5,4 +5,64 @@ describe("server", () => {
   it("runs the tests", () => {
     expect(true).toBe(true);
   });
+
+  describe("GET /users", () => {
+    it("returns 400 code if not logged in", () => {
+      return request(server)
+        .get("/api/users")
+        .then(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+    it("returns 401 code if invalid token", () => {
+      return request(server)
+        .get("/api/users")
+        .set("authorization", "token")
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+  });
+
+  describe("POST /register", () => {
+    it("returns 400 code if missing user info", () => {
+      return request(server)
+        .post("/api/register")
+        .send({ username: "kyle" })
+        .then(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+    it("response formatted in json", () => {
+      let user = {
+        username: "kyle",
+        password: "kyle123"
+      };
+      return request(server)
+        .post("/api/register")
+        .send(user)
+        .then(res => {
+          expect(res.type).toMatch(/json/);
+        });
+    });
+  });
+
+  describe("POST /login", () => {
+    it("returns 400 code if missing user info", () => {
+      return request(server)
+        .post("/api/login")
+        .send({ username: "tony" })
+        .then(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+    it("response uses json", () => {
+      return request(server)
+        .post("/api/login")
+        .send({ username: "tony" })
+        .then(res => {
+          expect(res.type).toMatch(/json/);
+        });
+    });
+  });
 });
